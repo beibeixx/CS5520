316d4f8cc288a0f3dfc20f9a8892bee4fa12a9ec
beibeixx <shelly0424x@gmail.com>
2024-09-11 05:48:57 +0800

    add TextInput and make it a controlled component, add Text component to show the text user typed in

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import Header from './Components/Header';
+import { useState } from 'react';
 
 export default function App() {
+  const [text, setText] = useState('')
   const appName = "My app!"
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Header name={appName} />
+      <TextInput
+        placeholder="here is it"
+        autoCorrect={true}
+        keyboardType='default'
+        value={text}
+        style={{ borderBottomColor: "purple", borderBottomWidth: 2}}
+        onChangeText={function (changedText) {
+          setText(changedText);
+        }}
+      />
+      <Text>{text}</Text>
     </View>
   );
 }
