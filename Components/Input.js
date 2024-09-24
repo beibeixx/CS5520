136bb746ec5b904331a750d6c66d4776bdfa1093
beibeixx <shelly0424x@gmail.com>
2024-09-24 15:51:21 -0700

    Fix the codes that did not follow in class.(TA and Neda already knew my late commits and that should be fine.)

--- Components/Input.js
+++ Components/Input.js
@@ -1,14 +1,28 @@
-import { StyleSheet, Text, TextInput, View, Button, Modal, Alert, Image} from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+  Modal,
+  Alert,
+  Image,
+} from "react-native";
 import React from "react";
 import { useState } from "react";
 
-export default function Input({ textInputFocus, inputHandler, visibility, cancelHandler }) {
+export default function Input({
+  textInputFocus,
+  inputHandler,
+  visibility,
+  cancelHandler,
+}) {
   const [text, setText] = useState("");
   const [focus, setFocus] = useState(false);
   const [count, setCount] = useState(0);
   function handleConfirm() {
     inputHandler(text);
-    setText('');
+    setText("");
   }
 
   const handleCancel = () => {
@@ -28,41 +42,51 @@ export default function Input({ textInputFocus, inputHandler, visibility, cancel
   };
 
   return (
-    <Modal animationType="slide" visible={visibility}>
+    <Modal animationType="slide" visible={visibility} transparent={ture}>
       <View style={styles.container}>
-      <Image 
-          source={{uri: 'https://cdn-icons-png.flaticon.com/512/2617/2617812.png'}}
-          style={styles.image}
-          alt="Network image"
-        />
-        <Image 
-          source={require('../assets/target-icon.png')}
-          style={styles.image}
-          alt="Local image"
-        />
-        <TextInput
-          style={styles.input}
-          autoFocus={textInputFocus}
-          autoCorrect={true}
-          placeholder="Type something"
-          keyboardType="default"
-          value={text}
-          onChangeText={(changeText) => {
-            setText(changeText);
-            setCount(changeText.length);
-          }}
-          onFocus={() => setFocus(true)}
-          onBlur={() => setFocus(false)}
-        />
-        {focus && count > 0 && <Text>{count}</Text>}
-        {!focus && count > 0 && (
-          <Text>
-            {count >= 3 ? "Thank you" : "Please type more than 3 characters"}
-          </Text>
-        )}
-        <View style={styles.buttonContainer}>
-          <Button title="Cancel" onPress={handleCancel} />
-          <Button title="Confirm" onPress={handleConfirm} disabled={count < 3}/>
+        <View style={styles.modalContainer}>
+          <Image
+            source={{
+              uri: "https://cdn-icons-png.flaticon.com/512/2617/2617812.png",
+            }}
+            style={styles.image}
+            alt="Image of a an arrow"
+          />
+          <Image
+            source={require("../assets/target-icon.png")}
+            style={styles.image}
+            alt="Image of a an arrow"
+          />
+          <TextInput
+            style={styles.input}
+            autoFocus={textInputFocus}
+            autoCorrect={true}
+            placeholder="Type something"
+            keyboardType="default"
+            value={text}
+            onChangeText={(changeText) => {
+              setText(changeText);
+              setCount(changeText.length);
+            }}
+            onFocus={() => setFocus(true)}
+            onBlur={() => setFocus(false)}
+          />
+          {focus && count > 0 && <Text>{count}</Text>}
+          {!focus && count > 0 && (
+            <Text>
+              {count >= 3 ? "Thank you" : "Please type more than 3 characters"}
+            </Text>
+          )}
+          <View style={styles.buttonsRow}>
+            <View style={styles.buttonContainer}>
+              <Button title="Cancel" onPress={handleCancel} />
+              <Button
+                title="Confirm"
+                onPress={handleConfirm}
+                disabled={count < 3}
+              />
+            </View>
+          </View>
         </View>
       </View>
     </Modal>
@@ -72,21 +96,30 @@ export default function Input({ textInputFocus, inputHandler, visibility, cancel
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
+    // backgroundColor: "white",
     alignItems: "center",
     justifyContent: "center",
   },
   input: {
-    borderColor: "gray",
+    borderColor: "purple",
     borderWidth: 2,
-    padding: 10,
-  },  
+    padding: 5,
+    marginVertical: 10,
+  },
+  modalContainer: {
+    borderRadius: 6,
+    backgroundColor: "#999",
+    alignItems: "center",
+  },
   buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    width: "30%",
+    margin: 10,
+  },
+  buttonsRow: {
+    flexDirection: "row",
   },
   image: {
     width: 100,
     height: 100,
-  }
+  },
 });
