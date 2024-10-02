42d522c900556ef965bda38c51840315d1b7a968
beibeixx <shelly0424x@gmail.com>
2024-10-01 17:48:15 -0700

    Change the component that for navigation header styles.

--- App.js
+++ App.js
@@ -10,28 +10,26 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Group
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: "purple",
-            },
-            headerTintColor: "white",
-          }}
-        >
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{ title: "My Goals" }}
-          />
-          <Stack.Screen
-            name="Details"
-            component={GoalDetails}
-            options={({ route }) => ({
-              title: route.params ? route.params.goalData.text : "More details",
-            })}
-          />
-        </Stack.Group>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "purple",
+          },
+          headerTintColor: "white",
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: "My Goals" }}
+        />
+        <Stack.Screen
+          name="Details"
+          component={GoalDetails}
+          options={({ route }) => ({
+            title: route.params ? route.params.goalData.text : "More details",
+          })}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
