1363c6a50b27728c53fe452f03beba3ef6a0eeaa
beibeixx <shelly0424x@gmail.com>
2024-10-22 14:10:38 -0700

    add user names in Flatlist in detail pages

--- Components/GoalUsers.js
+++ Components/GoalUsers.js
@@ -1,12 +1,29 @@
-import { StyleSheet, Text, useAnimatedValue, View } from "react-native";
-import React, { useEffect } from "react";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  useAnimatedValue,
+  View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 
 export default function GoalUsers() {
+  const [users, setUsers] = useState([]);
   useEffect(() => {
     async function fetchData() {
       try {
         const response = await fetch(
           "https://jsonplaceholder.typicode.com/users"
+        );
+        if (!response.ok) {
+          throw new Error(`An HTTP error happened status: ${response.status}`);
+        }
+        const data = await response.json();
+        // console.log(data[0].name);
+        setUsers(
+          data.map((users) => {
+            return users.name;
+          })
         );
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -14,9 +31,12 @@ export default function GoalUsers() {
     }
     fetchData();
   }, []);
+
   return (
     <View>
-      <Text>GoalUsers</Text>
+      <FlatList data={users} renderItem={({item}) => {
+        return <Text>{item}</Text>
+      }}/>
     </View>
   );
 }
