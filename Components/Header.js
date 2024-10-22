0a8e02551bca3c51e55b6074802d9e244c4343db
beibeixx <shelly0424x@gmail.com>
2024-10-22 14:58:17 -0700

    try dimension styles

--- Components/Header.js
+++ Components/Header.js
@@ -1,11 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  useWindowDimensions,
+} from "react-native";
 import React from "react";
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 export default function Header({ name }) {
-  console.log(name);
+  // console.log(name);
+  const { width, height } = useWindowDimensions();
+  const paddingVerticalDynamic = height < 415 ? 0 : 10;
   return (
     <View>
-      <Text style={styles.text}>Welcome to {name}!</Text>
+      <Text style={[styles.text, {paddingVertical: paddingVerticalDynamic}]}>Welcome to {name}!</Text>
     </View>
   );
 }
@@ -14,6 +24,7 @@ const styles = StyleSheet.create({
   text: {
     color: "darkblue",
     fontSize: 24,
+    //fontSize: windowWidth < 380 ? 20: 26,
     borderColor: "darkblue",
     borderWidth: 2,
     padding: 5,
