86eaf6c3cc772394c775dbfbfaf8343d890c66ca
beibeixx <shelly0424x@gmail.com>
2024-10-29 13:36:16 -0700

    installed AsyncStorage and initialized the firebase authentication.

--- Firebase/fireBaseSetup.js
+++ Firebase/fireBaseSetup.js
@@ -2,6 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeAuth , getReactNativePersistence} from 'firebase/auth'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,4 +23,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const database = getFirestore(app);
-// const analytics = getAnalytics(app);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});// const analytics = getAnalytics(app);
