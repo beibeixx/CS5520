5841f0c3e53d31874a60cd313f987d829f1dade3
beibeixx <shelly0424x@gmail.com>
2024-11-05 14:17:11 -0800

    Add the permission verification function for ios.

--- Components/ImageManager.js
+++ Components/ImageManager.js
@@ -1,24 +1,32 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Alert, Button, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import * as ImagePicker from "expo-image-picker";
 
 export default function ImageManager() {
   const [response, requestPermission] = ImagePicker.useCameraPermissions();
 
-  const verifyPermission = async() => {
-    if(!response.granted){
-        const result = await requestPermission();
-    }
-    return true;
-  }
+  const verifyPermission = async () => {
+    try {
+      if (response.granted) {
+        return true;
+      }
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    } catch (err) {}
+  };
 
   const takeImageHandler = async () => {
     try {
+      const hasPermission = await verifyPermission();
+      if (!hasPermission) {
+        Alert.alert("You dont have permission");
+        return;
+      }
       const result = await ImagePicker.launchCameraAsync({
         allowsEditing: true,
       });
     } catch (err) {
-      console.log("take image", err);
+      console.log("take image error", err);
     }
   };
 
